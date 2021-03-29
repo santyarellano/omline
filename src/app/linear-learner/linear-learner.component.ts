09566cd2123dce87f38f696dfc40c35b7643dfa4
santyarellano <santy.arellano039@gmail.com>
2021-03-29 13:18:51 -0600

    remove bias duplicate

--- src/app/linear-learner/linear-learner.component.ts
+++ src/app/linear-learner/linear-learner.component.ts
@@ -263,7 +263,7 @@ export class LinearLearnerComponent implements OnInit {
   update_model() {
     var temp_model = Object.assign({}, this.test_model);
     temp_model = this.prep_service.normalizeElement(temp_model);
-    temp_model["bias"] = 1;
+    temp_model["bias"] = 0;
     var hyp = this.hypothesys(temp_model);
     hyp = this.prep_service.denormalizeResult(hyp, this.predict_feature);
     this.test_result = hyp;
